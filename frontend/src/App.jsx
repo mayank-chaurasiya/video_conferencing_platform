79dea533d49ef7539df37238945423a3f0d82ffc
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-26 20:32:53 +0530

    Working on frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Landing_page from "./pages/landing_page/Landing_page.jsx";
+
+function App() {
+  return (
+    <>
+      <Landing_page />
+    </>
+  );
+}
+
+export default App;
