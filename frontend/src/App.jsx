92538484823cce5b4ea19b9f4821e6dd4d907044
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-26 21:02:10 +0530

    setting up frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Landing_page from "./pages/landing_page/Landing_page.jsx";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Landing_page />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing_page />} />
+        </Routes>
+      </Router>
     </>
   );
 }
